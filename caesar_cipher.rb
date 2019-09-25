f3eb227f3a66fb045bef83bfe3fac6e9e914b930
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-25 14:47:52 +0100

    initial commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,13 @@
+def caesar_cipher(str, int)
+    builder = ""
+    str.each_byte do |b|
+        if (b + int).between?(65,90) || (b + int).between?(97,122)
+            builder << (b + int).chr
+        elsif (b + int).between?(91,(90+int)) || (b + int).between?(123,(122+int))
+            builder << ((b + int)-26).chr
+        else
+            builder << b.chr
+        end
+    end
+    puts builder
+end
